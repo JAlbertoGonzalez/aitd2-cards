93dd458c37875601bb2f4ef9d654512d2c065dce
Alberto Torre <45895462+JAlbertoGonzalez@users.noreply.github.com>
2022-11-02 21:38:55 +0100

    Material UI

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,3 +1,4 @@
+import { Box, Card, MenuItem, Select, SelectChangeEvent, ToggleButton, ToggleButtonGroup, Typography } from '@mui/material'
 import React, { useEffect, useState } from 'react'
 import { getCardNames, getResult } from '../lib/utils'
 import { holes } from './constants'
@@ -17,22 +18,44 @@ function Main() {
 
     useEffect(() => updateCard(), [firstCard, overCard, hole])
 
-    return <div>
-        <p>{firstCard} over {overCard},<br />Hole number: {hole}</p>
-        <h3>Result: {resultString}</h3>
-        <p>First card:</p>
-        <select onChange={(e) => setFirstCard(e.target.value)}>
-            {cardNames.map((cardName, n) => <option key={n}>{cardName}</option>)}
-        </select>
-        <p>Over:</p>
-        <select onChange={(e) => setOverCard(e.target.value)}>
-            {cardNames.map((cardName, n) => <option key={n}>{cardName}</option>)}
-        </select>
+    return <Box>
+        <Card>
+            <Typography>
+                <p>{firstCard} over {overCard},<br />Hole number: {hole}</p>
+            </Typography>
+            <h3>Result: {resultString}</h3>
+            <p>First card:</p>
+            <Select onChange={(e: SelectChangeEvent<string>) => setFirstCard(e.target.value)}>
+                {cardNames.map((cardName, n) => {
+                    return <MenuItem key={n} value={cardName}>{cardName}</MenuItem>
+                })}
+            </Select>
+            <p>Over:</p>
+            <Select
+                placeholder='Select one...'
+                onChange={(e: SelectChangeEvent<string>) => setOverCard(e.target.value)}>
+                {cardNames.map((cardName, n) => {
+                    return <MenuItem
+                        key={n}
+                        value={cardName}>{cardName}</MenuItem>
+                })}
+            </Select>
 
-        <p>Hole number</p>
+            <p>Hole number</p>
 
-        {holes.map((number, n) => <button key={n} onClick={() => setHole(number)}>{number}</button>)}
-    </div>
+            <ToggleButtonGroup
+                value={hole}
+                exclusive
+                onChange={(e, value) => setHole(value)}>
+                {holes.map((number, n) => {
+                    return <ToggleButton
+                        value={number}
+                        key={n}>{number}</ToggleButton>
+                })}
+            </ToggleButtonGroup>
+
+        </Card>
+    </Box>
 }
 
 export default Main;
